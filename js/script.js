003de99168026085b1a1f758698b0d35772df457
gibranteoteo <muhammadgibrantoto@gmail.com>
2025-10-30 20:56:07 +0700

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   css/styles.css
    	new file:   index.html
    	new file:   js/script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//// Form Validation function
+function validateForm() {
+}
+
+//// Add Todo function
+function addTodo() {
+    
+}
+
+//// Delete and Filter functions
+function deleteTodo() {
+}
+
+//// Filter function
+function filterTodo() {
+}
